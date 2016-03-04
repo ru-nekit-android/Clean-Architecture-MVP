4c02aa24bcafb03faf93c57c1f41c865a1f3a6f7
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-04 16:06:25 +1000

    rename

--- app/src/main/java/ru/nekit/android/mvpmeeting/BaseMapper.java
+++ app/src/main/java/ru/nekit/android/mvpmeeting/BaseMapper.java
@@ -8,7 +8,7 @@ import rx.functions.Func1;
 /**
  * Created by MacOS on 04.03.16.
  */
-public abstract class MapperBase<T, R> implements Func1<List<T>, List<R>> {
+public abstract class BaseMapper<T, R> implements Func1<List<T>, List<R>> {
     @Override
     public List<R> call(List<T> inList) {
         return Observable.from(inList)
--- app/src/main/java/ru/nekit/android/mvpmeeting/presenter/mapper/RepositoryListMapper.java
+++ app/src/main/java/ru/nekit/android/mvpmeeting/presenter/mapper/RepositoryListMapper.java
@@ -1,13 +1,13 @@
 package ru.nekit.android.mvpmeeting.presenter.mapper;
 
-import ru.nekit.android.mvpmeeting.MapperBase;
+import ru.nekit.android.mvpmeeting.BaseMapper;
 import ru.nekit.android.mvpmeeting.model.data.RepositoryDTO;
 import ru.nekit.android.mvpmeeting.presenter.vo.Repository;
 
 /**
  * Created by MacOS on 04.03.16.
  */
-public class RepositoryListMapper extends MapperBase<RepositoryDTO, Repository> {
+public class RepositoryListMapper extends BaseMapper<RepositoryDTO, Repository> {
 
     public Repository convert(RepositoryDTO value) {
         return new Repository(value.getName(), value.getOwner().getLogin());
