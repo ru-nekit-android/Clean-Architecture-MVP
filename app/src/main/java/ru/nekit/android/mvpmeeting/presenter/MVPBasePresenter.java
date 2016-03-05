20241ea288a6e7b57fa01cfa29dd26afa21902db
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-05 23:29:52 +1000

    rename

--- app/src/main/java/ru/nekit/android/mvpmeeting/presenter/MVPBasePresenter.java
+++ app/src/main/java/ru/nekit/android/mvpmeeting/presenter/MVPBasePresenter.java
@@ -1,28 +1,34 @@
 package ru.nekit.android.mvpmeeting.presenter;
 
-import ru.nekit.android.mvpmeeting.model.IModel;
-import ru.nekit.android.mvpmeeting.model.Model;
+import android.os.Bundle;
+
+import ru.nekit.android.mvpmeeting.model.IMVPModel;
+import ru.nekit.android.mvpmeeting.model.GithubModel;
 import rx.Subscription;
 import rx.subscriptions.CompositeSubscription;
 
 /**
  * Created by ru.nekit.android on 04.03.16.
  */
-public abstract class BasePresenter implements IPresenter {
+public abstract class MVPBasePresenter {
 
-    protected final IModel model;
+    protected final IMVPModel model;
     private final CompositeSubscription subscriptionList;
 
-    protected BasePresenter() {
-        model = new Model();
+    protected MVPBasePresenter() {
+        model = createModel();
         subscriptionList = new CompositeSubscription();
     }
+
+    protected abstract IMVPModel createModel();
 
     protected void addSubscriber(Subscription subscription) {
         subscriptionList.add(subscription);
     }
 
-    @Override
+    public abstract void onLoadState(Bundle savedState);
+    public abstract void onSaveState(Bundle outSate);
+
     public void onStop() {
         subscriptionList.clear();
     }
