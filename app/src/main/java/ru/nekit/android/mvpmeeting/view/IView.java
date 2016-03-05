33460e72c2d7320f2e6743c991eefd2ed88d1512
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-05 23:34:17 +1000

    rename

--- app/src/main/java/ru/nekit/android/mvpmeeting/view/IView.java
+++
@@ -1,9 +0,0 @@
-package ru.nekit.android.mvpmeeting.view;
-
-/**
- * Created by MacOS on 02.03.16.
- */
-public interface IView {
-
-    void showError(String error);
-}
---
+++ app/src/main/java/ru/nekit/android/mvpmeeting/view/fragments/IMVPView.java
@@ -0,0 +1,9 @@
+package ru.nekit.android.mvpmeeting.view.fragments;
+
+/**
+ * Created by MacOS on 02.03.16.
+ */
+public interface IMVPView {
+
+    void showError(String error);
+}
