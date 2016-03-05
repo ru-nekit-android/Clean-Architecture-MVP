0622d2ed5e39a6cadb0eda45d20254add76835ec
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-05 13:31:18 +1000

    move functions to implementations

--- app/src/main/java/ru/nekit/android/mvpmeeting/view/IView.java
+++ app/src/main/java/ru/nekit/android/mvpmeeting/view/IView.java
@@ -1,19 +1,9 @@
 package ru.nekit.android.mvpmeeting.view;
 
-import java.util.List;
-
-import ru.nekit.android.mvpmeeting.presenter.vo.Repository;
-
 /**
  * Created by MacOS on 02.03.16.
  */
 public interface IView {
-
-    String getUserName();
-
-    void showRepositoryList(List<Repository> repositoryList);
-
-    void showEmptyList();
 
     void showError(String error);
 }
