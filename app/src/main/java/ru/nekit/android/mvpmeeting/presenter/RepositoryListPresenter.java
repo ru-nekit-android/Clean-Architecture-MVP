c2ac1a5122ffe12c028070881b7c76bfbe35e865
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-06 15:21:13 +1000

    rewrite Repository presenter according new interface and base presenter class

--- app/src/main/java/ru/nekit/android/mvpmeeting/presenter/RepositoryListPresenter.java
+++ app/src/main/java/ru/nekit/android/mvpmeeting/presenter/RepositoryListPresenter.java
@@ -3,14 +3,14 @@ package ru.nekit.android.mvpmeeting.presenter;
 import android.os.Bundle;
 import android.text.TextUtils;
 
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 import ru.nekit.android.mvpmeeting.model.GithubModel;
-import ru.nekit.android.mvpmeeting.model.IMVPModel;
+import ru.nekit.android.mvpmeeting.presenter.base.MVPBasePresenter;
 import ru.nekit.android.mvpmeeting.presenter.mapper.RepositoryListMapper;
 import ru.nekit.android.mvpmeeting.presenter.vo.Repository;
+import ru.nekit.android.mvpmeeting.utils.rx.RxTransformers;
 import ru.nekit.android.mvpmeeting.view.fragments.IRepositoryListView;
 
 /**
@@ -21,35 +21,47 @@ public class RepositoryListPresenter extends MVPBasePresenter {
     private static final String BUNDLE_REPOSITORY_LIST_KEY = "repository_list_key";
 
     private final RepositoryListMapper mMapper = new RepositoryListMapper();
-    private WeakReference<IRepositoryListView> mViewRef;
     private List<Repository> mRepositoryList;
 
-    public RepositoryListPresenter(IRepositoryListView view) {
-        super();
-        this.mViewRef = new WeakReference<>(view);
+    public RepositoryListPresenter() {
+        super(new GithubModel());
     }
 
     private GithubModel getModel() {
         return (GithubModel) model;
     }
 
+    private IRepositoryListView getRepositoryListView() {
+        return (IRepositoryListView) getView();
+    }
+
     public void onSearchClick() {
-        if (mViewRef != null) {
-            IRepositoryListView view = mViewRef.get();
-            if (view != null) {
-                String userName = view.getUserName();
-                if (TextUtils.isEmpty(userName)) return;
-                addSubscriber(getModel().getRepoList(userName).map(mMapper).subscribe(result -> {
-                    if (result != null && !result.isEmpty()) {
-                        mRepositoryList = result;
-                        view.showRepositoryList(result);
-                    } else {
-                        view.showEmptyList();
-                    }
-                }, error -> {
-                    view.showError(error.getMessage());
-                }));
-            }
+        IRepositoryListView view = getRepositoryListView();
+        if (isAttached()) {
+            String userName = view.getUserName();
+            if (TextUtils.isEmpty(userName)) return;
+            addSubscriber(getModel().getRepoList(userName)
+                    .map(mMapper)
+                    .compose(RxTransformers.applyOperationBeforeAndAfter((Runnable) () -> {
+                        if (isAttached()) {
+                            view.setData(null);
+                            view.showContent();
+                            view.showLoading();
+                        }
+                    }, (Runnable) () -> {
+                        if (isAttached()) {
+                            view.hideLoading();
+                        }
+                    }))
+                    .subscribe(result -> {
+                        if (result != null && !result.isEmpty()) {
+                            mRepositoryList = result;
+                            view.setData(result);
+                            view.showContent();
+                        } else {
+                            view.showEmptyList();
+                        }
+                    }, view::showError));
         }
     }
 
@@ -57,35 +69,28 @@ public class RepositoryListPresenter extends MVPBasePresenter {
         return mRepositoryList == null || mRepositoryList.isEmpty();
     }
 
-    @Override
-    protected IMVPModel createModel() {
-        return new GithubModel();
-    }
-
-    public void onLoadState(Bundle savedState) {
+    public void onCreate(Bundle savedState) {
+        IRepositoryListView view = getRepositoryListView();
         if (savedState != null) {
             mRepositoryList = (List<Repository>) savedState.getSerializable(BUNDLE_REPOSITORY_LIST_KEY);
-            if (mViewRef != null) {
-                IRepositoryListView view = mViewRef.get();
-                if (view != null) {
-                    view.showRepositoryList(mRepositoryList);
-                }
+            if (isAttached()) {
+                view.setData(mRepositoryList);
+                view.showContent();
             }
+        } else {
+            view.showEmptyList();
         }
     }
 
-    public void onSaveState(Bundle outState) {
+    public void onSaveInstanceState(Bundle outState) {
         if (!isRepoListEmpty()) {
             outState.putSerializable(BUNDLE_REPOSITORY_LIST_KEY, new ArrayList<>(mRepositoryList));
         }
     }
 
     public void selectRepository(Repository repository) {
-        if (mViewRef != null) {
-            IRepositoryListView view = mViewRef.get();
-            if (view != null) {
-                view.showRepository(repository);
-            }
+        if (isAttached()) {
+            getRepositoryListView().showRepository(repository);
         }
     }
 }
