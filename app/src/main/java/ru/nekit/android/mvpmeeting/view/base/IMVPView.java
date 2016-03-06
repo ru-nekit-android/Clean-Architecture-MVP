cc6b7f687b9d7753250fe872d8fbca2ba08a51b7
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-06 15:21:57 +1000

    MVP View interface stub

---
+++ app/src/main/java/ru/nekit/android/mvpmeeting/view/base/IMVPView.java
@@ -0,0 +1,7 @@
+package ru.nekit.android.mvpmeeting.view.base;
+
+/**
+ * Created by MacOS on 02.03.16.
+ */
+public interface IMVPView {
+}
--- app/src/main/java/ru/nekit/android/mvpmeeting/view/fragments/IMVPView.java
+++
@@ -1,9 +0,0 @@
-package ru.nekit.android.mvpmeeting.view.fragments;
-
-/**
- * Created by MacOS on 02.03.16.
- */
-public interface IMVPView {
-
-    void showError(String error);
-}
