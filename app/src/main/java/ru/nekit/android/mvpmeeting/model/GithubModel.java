32792b3b74c8add9ee2a1ab48fa09250a4109a8a
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-06 15:18:22 +1000

    use rxjava transform for support work in the background

--- app/src/main/java/ru/nekit/android/mvpmeeting/model/GithubModel.java
+++ app/src/main/java/ru/nekit/android/mvpmeeting/model/GithubModel.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import ru.nekit.android.mvpmeeting.model.api.ApiInterface;
 import ru.nekit.android.mvpmeeting.model.api.ApiModule;
+import ru.nekit.android.mvpmeeting.model.base.IMVPModel;
 import ru.nekit.android.mvpmeeting.model.data.RepositoryDTO;
+import ru.nekit.android.mvpmeeting.utils.rx.RxTransformers;
 import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by ru.nekit.android on 02.03.16.
@@ -17,6 +17,6 @@ public class GithubModel implements IMVPModel {
     ApiInterface apiInterface = ApiModule.getApiInterface();
 
     public Observable<List<RepositoryDTO>> getRepoList(String name) {
-        return apiInterface.getRepositories(name).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        return apiInterface.getRepositories(name).compose(RxTransformers.applySchedulers());
     }
 }
