ae2f0f4b5792dfcdb786143886d0f7078387c776
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-08 16:10:02 +1000

    implements interface by class

--- app/src/main/java/ru/nekit/android/mvpmeeting/model/GithubModel.java
+++ app/src/main/java/ru/nekit/android/mvpmeeting/model/GithubModel.java
@@ -1,22 +1,18 @@
 package ru.nekit.android.mvpmeeting.model;
 
-import java.util.List;
-
 import ru.nekit.android.mvpmeeting.model.api.ApiInterface;
 import ru.nekit.android.mvpmeeting.model.api.ApiModule;
 import ru.nekit.android.mvpmeeting.model.base.IMVPModel;
-import ru.nekit.android.mvpmeeting.model.data.RepositoryDTO;
-import ru.nekit.android.mvpmeeting.utils.rx.RxTransformers;
-import rx.Observable;
 
 /**
  * Created by ru.nekit.android on 02.03.16.
  */
-public class GithubModel implements IMVPModel {
+public class GithubModel implements IGithubModel {
 
-    ApiInterface apiInterface = ApiModule.getApiInterface();
+    private ApiInterface mApiInterface = ApiModule.getApiInterface();
 
-    public Observable<List<RepositoryDTO>> getRepoList(String name) {
-        return apiInterface.getRepositories(name).compose(RxTransformers.applySchedulers());
+    public ApiInterface getApiInterface() {
+        return mApiInterface;
     }
+
 }
