c5d07fd4752327eb0cd16228aec6ddc54fa83662
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-08 23:58:00 +1000

    remove unused

--- app/src/main/java/ru/nekit/android/mvpmeeting/view/fragments/RepositoryListFragment.java
+++ app/src/main/java/ru/nekit/android/mvpmeeting/view/fragments/RepositoryListFragment.java
@@ -50,11 +50,6 @@ public class RepositoryListFragment extends MVPBaseFragment<RepositoryListPresen
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
