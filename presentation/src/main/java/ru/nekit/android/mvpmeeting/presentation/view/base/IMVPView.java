d2274262cc3b3ce98dcffee3a65aaae919f064f7
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-18 23:44:15 +1000

    view has view model

--- presentation/src/main/java/ru/nekit/android/mvpmeeting/presentation/view/base/IMVPView.java
+++ presentation/src/main/java/ru/nekit/android/mvpmeeting/presentation/view/base/IMVPView.java
@@ -1,7 +1,12 @@
 package ru.nekit.android.mvpmeeting.presentation.view.base;
 
+import ru.nekit.android.mvpmeeting.presentation.model.base.IViewModel;
+
 /**
  * Created by MacOS on 02.03.16.
  */
-public interface IMVPView {
+public interface IMVPView<M extends IViewModel> {
+
+    M getModel();
+
 }
