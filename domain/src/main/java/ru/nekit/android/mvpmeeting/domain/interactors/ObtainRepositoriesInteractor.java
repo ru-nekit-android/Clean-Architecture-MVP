158097c396f6df921a0990861bc7d86ed3fdaf5a
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-19 19:05:39 +1000

    "middle man" between emitter and subscription
    http://blog.bradcampbell.nz/keep-your-main-thread-synchronous/

--- domain/src/main/java/ru/nekit/android/mvpmeeting/domain/interactors/ObtainRepositoriesInteractor.java
+++ domain/src/main/java/ru/nekit/android/mvpmeeting/domain/interactors/ObtainRepositoriesInteractor.java
@@ -6,6 +6,8 @@ import ru.nekit.android.mvpmeeting.domain.Repository;
 import ru.nekit.android.mvpmeeting.domain.interactors.base.IInteractor;
 import ru.nekit.android.mvpmeeting.domain.repository.IGithubRepository;
 import rx.Observable;
+import rx.Subscription;
+import rx.subjects.ReplaySubject;
 
 /**
  * Created by ru.nekit.android on 07.03.16.
@@ -13,12 +15,18 @@ import rx.Observable;
 public class ObtainRepositoriesInteractor implements IInteractor<List<Repository>, String> {
 
     private IGithubRepository mRepository;
+    private ReplaySubject<List<Repository>> mDelegateSubject;
+    private Subscription mDelegateSubscription;
 
     public ObtainRepositoriesInteractor(IGithubRepository repository) {
         mRepository = repository;
     }
 
     public Observable<List<Repository>> execute(String name) {
-        return mRepository.getRepositories(name);
+        if (mDelegateSubscription == null || mDelegateSubscription.isUnsubscribed()) {
+            mDelegateSubject = ReplaySubject.create();
+            mDelegateSubscription = mRepository.getRepositories(name).subscribe(mDelegateSubject);
+        }
+        return mDelegateSubject.asObservable();
     }
 }
