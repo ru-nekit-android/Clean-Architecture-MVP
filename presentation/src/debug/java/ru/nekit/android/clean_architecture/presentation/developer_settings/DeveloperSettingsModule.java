c79e23dc64ecad26ac6895b735cf3a32691b127d
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-29 22:26:40 +1000

    gitignore update

---
+++ presentation/src/debug/java/ru/nekit/android/clean_architecture/presentation/developer_settings/DeveloperSettingsModule.java
@@ -0,0 +1,10 @@
+package ru.nekit.android.clean_architecture.presentation.developer_settings;
+
+import dagger.Module;
+
+/**
+ * Created by ru.nekit.android on 29.03.16.
+ */
+@Module
+public class DeveloperSettingsModule {
+}
