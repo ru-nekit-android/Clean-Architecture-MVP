4ba2bd2b5bcc16d4f2001563d007b36fe8bceb40
ru.nekit.android <ru.nekit.android@gmail.com>
2016-03-30 18:39:00 +1000

    rename

--- presentation/src/main/java/ru/nekit/android/clean_architecture/presentation/model/base/IMVPModel.java
+++ presentation/src/main/java/ru/nekit/android/clean_architecture/presentation/model/base/IMVPModel.java
@@ -3,6 +3,6 @@ package ru.nekit.android.clean_architecture.presentation.model.base;
 /**
  * Created by ru.nekit.android on 02.03.16.
  */
-public interface IModel {
+public interface IMVPModel {
 
 }
