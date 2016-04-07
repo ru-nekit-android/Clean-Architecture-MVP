b92c0a0001b18464c2150f0c5450e85c929f2f77
ru.nekit.android <ru.nekit.android@gmail.com>
2016-04-07 17:50:14 +1000

    remove unused from application component

--- presentation/src/main/java/ru/nekit/android/clean_architecture/presentation/di/ApplicationComponent.java
+++ presentation/src/main/java/ru/nekit/android/clean_architecture/presentation/di/ApplicationComponent.java
@@ -5,14 +5,12 @@ import android.support.annotation.NonNull;
 import javax.inject.Singleton;
 
 import dagger.Component;
-import ru.nekit.android.clean_architecture.domain.repository.IGithubRepository;
 import ru.nekit.android.clean_architecture.presentation.GithubApplication;
 import ru.nekit.android.clean_architecture.presentation.developer_settings.DeveloperSettingsComponent;
 import ru.nekit.android.clean_architecture.presentation.developer_settings.DeveloperSettingsModule;
 import ru.nekit.android.clean_architecture.presentation.developer_settings.ILeakCanaryProxy;
 import ru.nekit.android.clean_architecture.presentation.di.modules.ApplicationModule;
 import ru.nekit.android.clean_architecture.presentation.di.modules.RepositoryListModule;
-import ru.nekit.android.clean_architecture.presentation.view.other.IViewModifier;
 
 /**
  * Created by ru.nekit.android on 08.03.16.
@@ -29,17 +27,11 @@ public interface ApplicationComponent {
     void inject(@NonNull GithubApplication value);
 
     @NonNull
-    ILeakCanaryProxy leakCanaryProxy();
-
-    @NonNull
-    IViewModifier viewModifier();
-
-    @NonNull
     DeveloperSettingsComponent developerSettingsComponent();
 
     @NonNull
-    IGithubRepository githubRepository();
+    RepositoryListComponent plus(@NonNull RepositoryListModule module);
 
     @NonNull
-    RepositoryListComponent plus(@NonNull RepositoryListModule module);
+    ILeakCanaryProxy leakCanaryProxy();
 }
