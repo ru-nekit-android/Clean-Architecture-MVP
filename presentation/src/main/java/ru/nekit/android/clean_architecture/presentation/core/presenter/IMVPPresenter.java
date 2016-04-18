6f3bbf23ff65d2f1341b9c210012f5f2be5a2ddf
ru.nekit.android <ru.nekit.android@gmail.com>
2016-04-18 18:34:18 +1000

    annotate with UiThread

--- presentation/src/main/java/ru/nekit/android/clean_architecture/presentation/core/presenter/IMVPPresenter.java
+++ presentation/src/main/java/ru/nekit/android/clean_architecture/presentation/core/presenter/IMVPPresenter.java
@@ -3,6 +3,7 @@ package ru.nekit.android.clean_architecture.presentation.core.presenter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
 
 import ru.nekit.android.clean_architecture.presentation.core.model.IMVPViewModel;
 import ru.nekit.android.clean_architecture.presentation.core.view.IMVPView;
@@ -12,21 +13,28 @@ import ru.nekit.android.clean_architecture.presentation.core.view.IMVPView;
  */
 public interface IMVPPresenter<V extends IMVPView, VM extends IMVPViewModel> {
 
+    @UiThread
     void attachView(@NonNull V view);
 
+    @UiThread
     void detachView();
 
+    @UiThread
     @Nullable
     V getView();
 
     @Nullable
     VM getViewModel();
 
+    @UiThread
     void onCreate(@Nullable Bundle savedInstanceState);
 
+    @UiThread
     void onSaveInstanceState(@Nullable Bundle bundle);
 
+    @UiThread
     void onDestroy();
 
-    void onAttachView();
+    @UiThread
+    void onAttachView(@NonNull V view);
 }
