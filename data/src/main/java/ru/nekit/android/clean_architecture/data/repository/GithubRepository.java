b444d9f54737d22158fc71720abbd267fc7e902d
ru.nekit.android <ru.nekit.android@gmail.com>
2016-04-21 15:26:48 +1000

    no need mapper

--- data/src/main/java/ru/nekit/android/clean_architecture/data/repository/GithubRepository.java
+++ data/src/main/java/ru/nekit/android/clean_architecture/data/repository/GithubRepository.java
@@ -5,9 +5,8 @@ import android.support.annotation.NonNull;
 import java.util.List;
 
 import ru.nekit.android.clean_architecture.data.di.api.GithubModule;
-import ru.nekit.android.clean_architecture.data.mapper.RepositoryEntityToRepositoryMapper;
 import ru.nekit.android.clean_architecture.data.utils.rx.RxTransformers;
-import ru.nekit.android.clean_architecture.domain.Repository;
+import ru.nekit.android.clean_architecture.domain.entities.RepositoryEntity;
 import ru.nekit.android.clean_architecture.domain.repository.IGithubRepository;
 import rx.Observable;
 import rx.Scheduler;
@@ -20,21 +19,19 @@ public class GithubRepository implements IGithubRepository {
     private final GithubModule.Api mGithubApi;
     private final Scheduler mLongOperationThread;
     private final Scheduler mMainThread;
-    private final RepositoryEntityToRepositoryMapper mMapper;
 
-    public GithubRepository(GithubModule.Api githubApi, RepositoryEntityToRepositoryMapper mapper,
+    public GithubRepository(GithubModule.Api githubApi,
                             Scheduler longOperationThread,
                             Scheduler mainThread) {
         mGithubApi = githubApi;
         mLongOperationThread = longOperationThread;
         mMainThread = mainThread;
-        mMapper = mapper;
     }
 
     @Override
     @NonNull
-    public Observable<List<Repository>> getRepositories(final String userName) {
-        return mGithubApi.getRepositories(userName).map(mMapper)
+    public Observable<List<RepositoryEntity>> getRepositories(final String userName) {
+        return mGithubApi.getRepositories(userName)
                 .compose(RxTransformers.applySchedulers(mLongOperationThread, mMainThread));
     }
 }
