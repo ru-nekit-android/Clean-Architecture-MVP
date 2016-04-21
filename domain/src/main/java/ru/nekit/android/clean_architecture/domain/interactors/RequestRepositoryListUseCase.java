6e85793ca11db4b883bc608a541cecf3ab242ab6
ru.nekit.android <ru.nekit.android@gmail.com>
2016-04-21 15:26:19 +1000

    use case changes

--- domain/src/main/java/ru/nekit/android/clean_architecture/domain/interactors/RequestRepositoryListUseCase.java
+++ domain/src/main/java/ru/nekit/android/clean_architecture/domain/interactors/RequestRepositoryListUseCase.java
@@ -1,10 +1,9 @@
 package ru.nekit.android.clean_architecture.domain.interactors;
 
-
 import java.util.List;
 
-import ru.nekit.android.clean_architecture.domain.Repository;
 import ru.nekit.android.clean_architecture.domain.core.interactors.IInteractor;
+import ru.nekit.android.clean_architecture.domain.entities.RepositoryEntity;
 import ru.nekit.android.clean_architecture.domain.repository.IGithubRepository;
 import rx.Observable;
 import rx.Subscription;
@@ -13,17 +12,17 @@ import rx.subjects.ReplaySubject;
 /**
  * Created by ru.nekit.android on 07.03.16.
  */
-public class RequestRepositoryListUseCase implements IInteractor<List<Repository>, String> {
+public class RequestRepositoryListUseCase implements IInteractor<List<RepositoryEntity>, String> {
 
     private final IGithubRepository mRepository;
-    private ReplaySubject<List<Repository>> mDelegateSubject;
+    private ReplaySubject<List<RepositoryEntity>> mDelegateSubject;
     private Subscription mDelegateSubscription;
 
     public RequestRepositoryListUseCase(IGithubRepository repository) {
         mRepository = repository;
     }
 
-    public Observable<List<Repository>> execute(String name) {
+    public Observable<List<RepositoryEntity>> execute(String name) {
         if (mDelegateSubscription == null || mDelegateSubscription.isUnsubscribed()) {
             mDelegateSubject = ReplaySubject.create();
             mDelegateSubscription = mRepository.getRepositories(name).subscribe(mDelegateSubject);
